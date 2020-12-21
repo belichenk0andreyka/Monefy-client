68f2f144d417839060849e57242dc0e4b7eef4f5
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2020-12-21 23:59:28 +0200

    make getting currencies course

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -5,11 +5,13 @@ import {reducer as toastrReducer} from 'react-redux-toastr'
 import uiReducer from 'store/reducers/uiReducer';
 import userReducer from 'store/reducers/userReducer';
 import actionsReducer from 'store/reducers/actionsReducer';
+import currenciesReducer from 'store/reducers/currenciesReducer';
 
 const rootReducer = (history) => combineReducers({
     user: userReducer,
     ui: uiReducer,
     actions: actionsReducer,
+    currencies: currenciesReducer,
     toastr: toastrReducer,
     router: connectRouter(history),
 })
